0877ea0ffc2ca3b39fe6174d8dd25f4f37751661
Rowan Walton <rowanwalton0@gmail.com>
2023-08-10 13:14:52 +0200

    saving run files

--- paddy/app.py
+++ paddy/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("/index.html")
+
+@app.route("/sightings", methods=["POST", "GET"])
+def sightings():
+    if request.method == "POST":
+        return render_template("/sightings.html")
+    else:
+        map_location = request.form.get("map_location")
+        current_location = request.form.get("current_location")
+    
